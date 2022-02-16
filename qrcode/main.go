ab3339233c4ab2e5011c2e1816e888b2b0d24de5
Philip Schlump <pschlump@gmail.com>
2022-02-16 10:55:06 -0700

    Added tests for svg, txt and .png

--- qrcode/main.go
+++ qrcode/main.go
@@ -9,14 +9,19 @@ import (
 	"os"
 	"strings"
 
+	svg "github.com/ajstarks/svgo"
+	"github.com/boombuler/barcode/qr"
+	"github.com/pschlump/filelib"
 	"github.com/pschlump/goqrcode"
+	"github.com/pschlump/goqrsvg"
 )
 
 func main() {
 
 	outFile := flag.String("o", "", "out PNG file prefix, empty for stdout")
 	size := flag.Int("s", 256, "image size (pixel)")
-	textArt := flag.Bool("t", false, "print as text-art on stdout")
+	textArt := flag.Bool("txt", false, "print as text-art on stdout")
+	svgArt := flag.Bool("svg", false, "print as text-art on stdout")
 	negative := flag.Bool("i", false, "invert black and white")
 	level := flag.String("l", "h", "Level of errro redundancey, h|m|l for high, medium, low")
 	flag.Usage = func() {
@@ -35,7 +40,8 @@ Example:
 Options:
 	-o File			Output file name (will add .png if you do not specify it)
 	-s ImageSize	Pixel size of image, 256 is the default.
-	-t				Paint as text art on stdout.
+	-txt				Paint as text art in output.
+	-svg				Paint as svg in output.
 	-i				Inert color - swap black and white
 	-l Level		Level is h, m, l for high, medium, low levesl of qr error correction
 	
@@ -47,6 +53,17 @@ Options:
 	if len(flag.Args()) != 1 {
 		flag.Usage()
 		goqrcode.CheckError(fmt.Errorf("Error: no text to encode into image"))
+	}
+
+	out := os.Stdout
+	if *outFile != "" {
+		var err error
+		out, err = filelib.Fopen(*outFile, "w")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to open %s for output: %s\n", *outFile, err)
+			os.Exit(1)
+		}
+		defer out.Close()
 	}
 
 	content := strings.Join(flag.Args(), " ")
@@ -71,8 +88,16 @@ Options:
 
 	if *textArt {
 		// Output the QR Code as a string
+		// TODO if *negative {
 		art := q.ToString(*negative)
-		fmt.Println(art)
+		fmt.Fprintf(out, art)
+		return
+	}
+
+	if *svgArt {
+		// TODO -------------------------------------------------------------------------------
+		// TODO if *negative {
+		genqr(content, out)
 		return
 	}
 
@@ -86,17 +111,19 @@ Options:
 	png, err = q.PNG(*size)
 	goqrcode.CheckError(err)
 
-	if *outFile == "" {
-		os.Stdout.Write(png)
-	} else {
-		var fh *os.File
-		s := *outFile
-		if !strings.HasSuffix(s, ".png") {
-			s += ".png"
-		}
-		fh, err = os.Create(s)
-		goqrcode.CheckError(err)
-		defer fh.Close()
-		fh.Write(png)
-	}
+	out.Write(png)
+}
+
+func genqr(uu string, fp *os.File) {
+	s := svg.New(fp)
+
+	// Create the QR Code in SVG
+	qrCode, _ := qr.Encode(uu, qr.M, qr.Auto)
+
+	// Write QR code to SVG
+	qs := goqrsvg.NewQrSVG(qrCode, 5)
+	qs.StartQrSVG(s)
+	qs.WriteQrSVG(s)
+
+	s.End()
 }
